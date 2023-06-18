176b5bb02744282a593b39ebbc23199d350cd1ae
andrey <andreyradin@gmail.com>
2023-06-18 03:17:11 +0300

    Добавил 6 урок

---
+++ 03_variables.py
@@ -0,0 +1,30 @@
+# number = 5
+# didgit = 6.987
+# word = 'Результат:'
+# boolean = True
+#
+# str_num = '5'
+# print(word, str(didgit))
+#
+# print(number + int(str_num))
+
+# print('Результат:', 5 ** 3)
+# print('Результат:', 5 // 3)
+# print('Результат:', min(7, 5, 4, -9, 2))
+# print('Результат:', max(7, 5, 4, -9, 2))
+# print('Результат:', abs(-9))
+# print('Результат:', pow(5, 3))
+# print('Результат:', round(5 / 3))
+# print('Результат: ', 7, 19, sep='', end='!\n')
+# print('Second l\ti\\n\ne')
+
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+num2 += 3
+print('Result:', num1 + num2)
+print('Result:', num1 - num2)
+print('Result:', num1 * num2)
+print('Result:', num1 / num2)
+print('Result:', num1 // num2)
+word = 'Hi'
+print(word * 2)
